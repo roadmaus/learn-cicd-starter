4c5a7ed9e2f0a97dda5c44874e893ed5a9013a2c
roadmaus <gufe@protonmail.com>
2025-09-16 12:05:43 +0200

    fixing auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -6,17 +6,17 @@ import (
 	"strings"
 )
 
-var ErrNoAuthHeaderIncluded = errors.New("nil")
+var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("nil")
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("nil")
+		return "", errors.New("malformed authorization header")
 	}
 
 	return splitAuth[1], nil
